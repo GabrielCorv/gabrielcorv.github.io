f42467094a491ac3c02d15f5561aff2c9ec1ad19
Gabriel Corv <96327135+GabrielCorv@users.noreply.github.com>
2023-02-23 19:59:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuVisible = false
-//função pra ocultar o menu
+
 function showHiddenMenu() {
   if (menuVisible) {
     document.getElementById('nav').classList = ''
@@ -13,4 +13,26 @@ function showHiddenMenu() {
 function select() {
   document.getElementById('nav').classList = ''
   menuVisible = false
+}
+
+function select() {
+  var skills = document.getElementById('skills')
+  var distance_bar = window.innerHeight - skills.getBoundingClientRect().top
+  if (distance_bar >= 400) {
+    let skill = document.getElementsByClassName('progress')
+    skill[0].classList.add('javascript')
+    skill[1].classList.add('htmlcss')
+    skill[2].classList.add('photoshop')
+    skill[3].classList.add('python')
+    skill[4].classList.add('sql')
+    skill[5].classList.add('comunication')
+    skill[6].classList.add('teamplay')
+    skill[7].classList.add('criative')
+    skill[8].classList.add('dedication')
+    skill[9].classList.add('management')
+  }
+}
+
+window.onscroll = function () {
+  select()
 }
